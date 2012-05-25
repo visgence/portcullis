e72bfe3da76bab7443f8643a6f0db0c43288fe08
Evan Salazar <esalazar1981@gmail.com>
2012-05-25 09:12:23 -0600

    Needed to register plugin

--- snmpPoller/admin.py
+++ snmpPoller/admin.py
@@ -1,8 +1,9 @@
 from snmpPoller.models import *
 from django.contrib import admin
 
-admin.site.register(Host)
-#admin.site.register(SnmpStream)
 
 class SnmpStreamAdmin:
     list_display = ('dataStream.id','host.hostname','oid')
+
+admin.site.register(Host)
+admin.site.register(SnmpStream,SnmpStreamAdmin)
