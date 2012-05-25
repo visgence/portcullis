d193039061b6bde4b78cf5068839fb284ec8a175
Evan Salazar <esalazar1981@gmail.com>
2012-05-25 09:22:08 -0600

    Needed to add methods for forigen keys

--- snmpPoller/admin.py
+++ snmpPoller/admin.py
@@ -3,7 +3,14 @@ from django.contrib import admin
 
 
 class SnmpStreamAdmin(admin.ModelAdmin):
-    list_display = ('dataStream.id','host.hostname','oid')
+    list_display = ('dataStream_id','host_hostname','oid')
+
+    def dataStream_id(self,obj):
+        return obj.dataStream.id
+    
+    def host_hostname(self,obj):
+        return obj.host.hostname
+
 
 admin.site.register(Host)
 admin.site.register(SnmpStream,SnmpStreamAdmin)
