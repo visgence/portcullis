9b6d7baab5783db72f18e12ea84de5a41b17bcb4
Evan Salazar <esalazar1981@gmail.com>
2012-05-24 20:26:30 -0600

    Started SNMP Poller Project

---
+++ snmpPoller/models.py
@@ -0,0 +1,25 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#     * Rearrange models' order
+#     * Make sure each model has one field with primary_key=True
+# Feel free to rename the models, but don't rename db_table values or field names.
+#
+# Also note: You'll have to insert the output of 'django-admin.py sqlcustom [appname]'
+# into your database.
+
+from django.db import models
+from portcullis.models import *
+
+
+class Host(models.Model):
+    hostname = models.CharField(max_length=64, unique=True, blank=False)
+    version = models.CharField(max_length=2, choices=(("1","1"),("2c","2c"),("3","3")))
+    community = models.CharField(max_length=64,blank=False)
+    def __unicode__(self):
+        return "Host: hostname=%s version=%s community=%s" (hostname,version,community)
+
+
+class SnmpStream(models.Model):
+    dataStream = models.ForeignKey(DataStream)
+    host = models.ForeignKey(Host)
+    oid = models.CharField(max_length=128)
