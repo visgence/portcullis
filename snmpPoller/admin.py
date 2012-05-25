24d24c80470e9347aa4c3d3e83537c91c41798cf
Evan Salazar <esalazar1981@gmail.com>
2012-05-25 09:09:51 -0600

    Testing admin look

--- snmpPoller/admin.py
+++ snmpPoller/admin.py
@@ -2,4 +2,7 @@ from snmpPoller.models import *
 from django.contrib import admin
 
 admin.site.register(Host)
-admin.site.register(SnmpStream)
+#admin.site.register(SnmpStream)
+
+class SnmpStreamAdmin:
+    list_display = ('dataStream.id','host.hostname','oid')
