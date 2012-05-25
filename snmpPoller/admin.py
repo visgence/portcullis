30081c37678c267c3357b30c34e6781c0d967e06
Evan Salazar <esalazar1981@gmail.com>
2012-05-25 09:13:43 -0600

    added ModelAdmin

--- snmpPoller/admin.py
+++ snmpPoller/admin.py
@@ -2,7 +2,7 @@ from snmpPoller.models import *
 from django.contrib import admin
 
 
-class SnmpStreamAdmin:
+class SnmpStreamAdmin(admin.ModelAdmin):
     list_display = ('dataStream.id','host.hostname','oid')
 
 admin.site.register(Host)
