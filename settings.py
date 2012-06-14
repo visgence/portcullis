e8c849ff87ca529e0190703432d706a2d1aebc75
David Salas <david@visgence.com>
2012-06-14 16:02:42 -0600

    added some admin tool customization

--- settings.py
+++ settings.py
@@ -114,7 +114,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    'template/'
+    'template/',
+    'portcullis/templates'
 )
 
 INSTALLED_APPS = (
