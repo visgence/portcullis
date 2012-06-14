2dd2fe2042aa8e478bd00a6e2e5d80dc784f77dc
Bretton Murphy <bmurphy@visgence.com>
2012-06-14 13:31:58 -0600

    added some comments to tests in portcullis

--- portcullis/tests.py
+++ portcullis/tests.py
@@ -27,6 +27,8 @@ class PermissionTest(TestCase):
 
         
     def test_device_by_key(self):
+        '''Can we get a device by a key'''
+
         myKey = Key.objects.validate("apple")
         self.assertEqual(myKey.key, "apple")
 
@@ -37,6 +39,8 @@ class PermissionTest(TestCase):
         self.assertEqual(myDevice.name, "device 1")
 
     def test_get_writable_by_device(self):
+        '''Get all streams that are writable by a given device'''
+
         myDevice = Device.objects.get(name = "device 2")
         dataStreams = DataStream.objects.get_writable_by_device(myDevice).order_by('name')
         self.assertEqual(dataStreams[0].name, "stream 2")
@@ -49,6 +53,8 @@ class PermissionTest(TestCase):
         self.assertEqual(dataStreams[2].name, "stream 3")
 
     def test_get_writable_by_key(self):
+        '''Get all streams that are writable by a given key'''
+
         myKey = Key.objects.validate('pear')
         dataStreams = DataStream.objects.get_writable_by_key(myKey)
         self.assertEquals(dataStreams[0].name, "stream 2")
