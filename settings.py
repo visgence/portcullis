020459098bc42b706e2babc700b081b124d54ff3
Evan Salazar <esalazar1981@gmail.com>
2012-06-28 18:37:04 -0600

    Changed default db to sqlite3

--- settings.py
+++ settings.py
@@ -15,10 +15,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'portcullis',                      # Or path to database file if using sqlite3.
-        'USER': 'portcullis',                      # Not used with sqlite3.
-        'PASSWORD': 'p0rtcullis',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'portcullis.db',                      # Or path to database file if using sqlite3.
+        #'USER': 'portcullis',                      # Not used with sqlite3.
+        #'PASSWORD': 'p0rtcullis',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
