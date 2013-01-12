a3891b4b26954990be6cb6c169faa17ec3dddd06
Jeremiah Davis <jdavis@visgence.com>
2013-01-12 14:30:34 -0700

    Fix merge conflicts, give it command line args.

--- genTestData.py
+++ genTestData.py
@@ -5,18 +5,65 @@ import os,sys
 sys.path.insert(0, os.path.expanduser('./'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE",'settings')
 
-from portcullis.models import DataStream
+from django.core.exceptions import ObjectDoesNotExist
+
+from portcullis.models import DataStream, ScalingFunction
 from collector.data_loader import insert_reading
 
 import math
 import time
+import random
+import sys
 
-ds = DataStream.objects.get(id=1)
-startTime = int(time.mktime(time.strptime("2013-01-01", "%Y-%m-%d")))
-for t in range(startTime, startTime + 60*60*24*31, 1000*60):
+strf = "%Y-%m-%d"
+
+try:
+    scaleF = ScalingFunction.objects.get(name='Identity')
+    print 'Scaling Function good.'
+except ObjectDoesNotExist:
+    print 'I am creating a scaling function'
+    scaleF = ScalingFunction(name = 'Identity', definition = 'return x;')
+    scaleF.save()
+    
+try:
+    ds1 = DataStream.objects.get(id=1)
+except ObjectDoesNotExist:
+    ds1 = DataStream(node_id = 1, port_id = 1, name = 'Test data 1', description = 'Simple Sign curve',
+                     color = 'purple', min_value = -50.0, max_value = 50.0, scaling_function = scaleF)
+    ds1.save()
+
+try:
+    ds2 = DataStream.objects.get(id=2)
+except ObjectDoesNotExist:
+    ds2 = DataStream(node_id = 2, port_id = 2, name = 'Test data 2', description = 'Amplitude modulated sin curve.',
+                     color = 'red   ', min_value = -50.0, max_value = 50.0, scaling_function = scaleF)
+    ds2.save()
+
+try:
+    ds3 = DataStream.objects.get(id=3)
+except ObjectDoesNotExist:
+    ds3 = DataStream(node_id = 3, port_id = 3, name = 'Test data 3', description = 'Random amplitude modulation',
+                     color = 'blue', min_value = -50.0, max_value = 50.0, scaling_function = scaleF)
+    ds3.save()
+
+if len(sys.argv) > 1:
+    startTime = int(time.mktime(time.strptime(sys.argv[1], strf)))
+else:
+    print 'You must give a date in the format: YYYY-MM-DD'
+    sys.exit(1)
+
+if len(sys.argv) > 2:
+    nMonths = int(sys.argv[2])
+else:
+    nMonths = 1
+    
+for t in range(startTime, startTime + 60*60*24*31*6, 1000*60):
     print "Time = %s" % time.localtime(t)
+
     for i in range(t, t + 1000*60, 60):
-        insert_reading(ds, 50 * math.sin(i*math.pi/(86400)), i)
+        insert_reading(ds1, 50 * math.sin(i*math.pi/(86400)), i)
+        insert_reading(ds2, 50 * math.sin(i*2*math.pi/(60*60*24*30)) * math.sin(i*math.pi/(86400)), i)
+        insert_reading(ds3, random.randint(0, 50) * math.sin(i*math.pi/(86400)), i)
     time.sleep(1)
 
 print 'Done'
