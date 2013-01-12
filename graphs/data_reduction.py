7c7cab4d379a6aae8cf18f8f98e2d6a2ca5365df
Jeremiah Davis <jdavis@visgence.com>
2013-01-12 13:06:12 -0700

    Convert to float before operating to save time.

--- graphs/data_reduction.py
+++ graphs/data_reduction.py
@@ -3,9 +3,10 @@ def reduce_data(rawData, reduction_type = 'mean'):
     
     if(reduction_type == None or reduction_type == ""):
         reduction_type = 'mean'
-    
-    coord_data = mean([x[1] for x in rawData])
-    return [coord_time, float(coord_data)]
+
+    # convert to float before taking mean, because float operations are faster than decimal operations
+    coord_data = mean([float(x[1]) for x in rawData])
+    return [coord_time, coord_data]
 
 def mean(a):
     return sum(a)/len(a)
