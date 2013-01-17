d7fa17baa0b444eff3fe317e63cd5660388c886f
Jeremiah Davis <jdavis@visgence.com>
2013-01-17 14:18:41 -0700

    Remove admin link from portcullis app.

--- portcullis/urls.py
+++ portcullis/urls.py
@@ -11,12 +11,7 @@ urlpatterns = patterns('portcullis.login',
      url(r'^$', 'user_login'),
      url(r'^login/$', 'user_login'),
      url(r'^logout/$', 'logout'),
-     
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
 )
 
 urlpatterns += patterns('portcullis.user_portal',
