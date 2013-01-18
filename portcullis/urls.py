8357309eddc02e3fac8b785533f61e005191370d
Jeremiah Davis <jdavis@visgence.com>
2013-01-17 17:07:53 -0700

    Put redirect on /portcullis/ to /portcullis/user_streams to fix login_page issue.

--- portcullis/urls.py
+++ portcullis/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.simple import redirect_to
 
 urlpatterns = patterns('portcullis.views',
-     url(r'^$', 'login.user_login'),
+     url(r'^$', redirect_to, {'url': 'user_streams'}),
      url(r'^login/$', 'login.user_login'),
      url(r'^logout/$', 'login.logout'),
      url(r'^user_streams/$', 'user_portal.user_streams'),
