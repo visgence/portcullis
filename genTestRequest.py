a413b31a091fe9144e75e39ad88c742b7aa411ff
Jeremiah Davis <jdavis@visgence.com>
2013-01-18 18:22:44 -0700

    Update data upload tester to do in bulk.

--- genTestRequest.py
+++ genTestRequest.py
@@ -5,34 +5,41 @@ from urllib import urlencode, urlopen
 import math
 import time
 import random
+try: import simplejson as json
+except ImportError: import json
 
 strf = "%Y-%m-%d"
 
-#if len(sys.argv) > 1:
-#    startTime = int(time.mktime(time.strptime(sys.argv[1], strf)))
-#else:
-#    print 'You must give a date in the format: YYYY-MM-DD'
-#    sys.exit(1)
+if len(sys.argv) > 1:
+    startTime = int(time.mktime(time.strptime(sys.argv[1], strf)))
+else:
+    print 'You must give a date in the format: YYYY-MM-DD'
+    sys.exit(1)
 
-#if len(sys.argv) > 2:
-#    nMonths = int(sys.argv[2])
-#else:
-#    nMonths = 1
+if len(sys.argv) > 2:
+    nMonths = int(sys.argv[2])
+else:
+    nMonths = 1
 
 
 params = {'auth_token': 'I want my baby back ribs.'}
-#for t in range(startTime, startTime + 60*60*24*31*nMonths, 1000*60):
-while (True):
-    i = time.time()
-    print "Time = %s" % time.localtime(i)
+for t in range(startTime, startTime + 60*60*24*31*nMonths, 1000*60):
+    print "Time = %s" % time.localtime(t)
 
-    #for i in range(t, t + 1000*60, 60):
-    params['datastream_id'] = 1
-    params['value'] = 50 * math.sin(i*math.pi/(86400))
-    resp = urlopen('http://localhost:8000/collector/add_reading/?%s' % urlencode(params))
+    data = []
+
+    for i in range(t, t + 10*60, 60):
+        ds_id = 1
+        value =  50 * math.sin(i*math.pi/(86400))
+        data.append([ds_id, value, int(t)])
+                    
+
+    params['json'] = json.dumps(data)
+
+    resp = urlopen('http://localhost:8000/collector/add_reading_bulk/?%s' % urlencode(params))
     print 'Http Response: '
     print resp.readline()
     print
-    time.sleep(1)
+    time.sleep(.1)
 
 print 'Done'
