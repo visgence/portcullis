f5ab5b86078b42658f9c326c93e22ceb6f8c664a
Bretton Murphy <bmurphy@visgence.com>
2013-01-30 14:44:23 -0700

    Side pane stays fixed when user scrolls page now.

--- portcullis/static/js/layout.js
+++ portcullis/static/js/layout.js
@@ -1,4 +1,3 @@
-
 function toggle_form_section(event) {
     /*
      * This function toggles the corresponding sibling div of the header
@@ -10,5 +9,25 @@ function toggle_form_section(event) {
 }
 
 function collapse_all() {
+    /*
+     * Grabs all section toggles and hides their sibling element
+     */
+
     $('.section_toggle').next().hide();
+}
+
+function sidepane_relocate() {
+    /*
+     * Checks the side panes anchor and determines if the user has scrolled it 
+     * off the screen at the top.  If so make the side pane "sticky" and stay with the 
+     * user down the page.
+     */
+    
+    var window_top = $(window).scrollTop();
+    var div_top = $('#side_pane_anchor').offset().top;
+
+    if (window_top > div_top)
+        $('#side_pane').addClass('stick')
+    else
+        $('#side_pane').removeClass('stick'); 
 }
