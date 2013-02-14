e2de4c03e8e178d7a466e7fc8bd044cdec946755
Jeremiah Davis <jdavis@visgence.com>
2013-02-14 16:59:21 -0700

    Add ajax refresh, move into object (grid).

--- portcullis/templates/crud.js
+++ portcullis/templates/crud.js
@@ -13,81 +13,43 @@
  * KendoUI grid to manage models.  As long as this file takes to load, I think after development
  * is finished, it should be statically generated.
  */
-    {% endcomment %}
+{% endcomment %}
 
-
-var grid;
 var columns = {{columns|safe}};
 
 var options = {
     enableCellNavigation: true
- };
+};
 
-$(function() {
-    var data = {{data|safe}};
-
-    grid = new Slick.Grid("#{{model_name}}_grid", data, columns, options);
-    console.log(grid);
-});
-
-/** Setup the model kendo datasource.
-var dataSource = new kendo.data.DataSource({
-    error: function(e) {
-        alert(e.errors);
-        var grid = $('#{{model_name}}_grid').data('kendoGrid');
-        grid.cancelChanges();
-    },
-    transport: {
-        read: function(options){
-            console.log('In read');
-            Dajaxice.portcullis.read_source(function(response) {
-                options.success(response);}, { 'model_name': '{{model_name}}'});
+var dataModel = {
+    model: {
+        data: [],
+        getItem: function(i) {
+            return this.data[i];
         },
-        update: function(options){
-            Dajaxice.portcullis.update_model_obj(function(response) {
-                options.success(response);}, {'data': options.data, 'model_name':'{{model_name}}'});
+        getItemMetaData: function(i) {
+            return null;
         },
-        destroy: function(options){
-            Dajaxice.portcullis.destroy(function(response) {
-                options.success(response);
-            }, {'data':options.data});   
-        },
-        create: function(options) {
-            Dajaxice.portcullis.create_model_obj(function(response) {
-                options.success(response);}, {'data': options.data, 'model_name': '{{model_name}}'});
-        },
-        parameter: function(data, type) {
-            console.log('In parameter map.');
-            console.log(data);
-            console.log(type);
-            return {data: kendo.stringify(data)};
+        getLength: function() {
+            return this.data.length;
         }
     },
-    schema: {
-        data: function(d) {
-            return d;
-        },
-        errors: function(response) {
-            return response.errors;
-        },
-        model: {{ model|safe }}
-    }
-}); 
+    
+    grid: null,
 
-$(function() {    
-    $('#{{model_name}}_grid').kendoGrid({
-        dataSource: dataSource,
-        columns: {{ columns|safe }},
-        editable: 'popup',
-        navigable: true,
-        toolbar: ['create'],//, 'save', 'cancel'],
-        //editable: {
-        //    update: true,
-        //    destroy: false,
-        //    confirmation: "Do you really want to delete this item?  This operation cannot be undone."
-        //},
-        navigatable: true,
-        sortable: true
-    });
+    refresh: function() {
+        self = this;
+        Dajaxice.portcullis.read_source(
+            function(response) {
+                self.model.data = response;
+                self.grid.invalidate();
+            },
+            {model_name: '{{model_name}}'}
+        );
+    }
+};
+
+$(function() {
+    dataModel.grid = new Slick.Grid("#{{model_name}}_grid", dataModel.model, columns, options);
+    dataModel.refresh();
 });
-*/
