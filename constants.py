b36e8ae9a2a34d66b635fedb2face5c05c7001bf
Bretton Murphy <bmurphy@visgence.com>
2013-02-25 18:08:40 -0700

    Added a global constants file.

---
+++ constants.py
@@ -0,0 +1,15 @@
+"""
+" portcullis/globals.py
+" Contributing Authors:
+"    Bretton Murphy (Visgence, Inc)
+"
+" (c) 2013 Visgence, Inc.
+"""
+
+
+'''''''''''''''''''''''''''''''''''''''''''''''
+               Time Constansts
+'''''''''''''''''''''''''''''''''''''''''''''''
+
+#Format for datetime objects
+DT_FORMAT = "%m/%d/%Y %H:%M:%S"
