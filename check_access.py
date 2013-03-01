c91e3816303e2f8981f1fefad9d70e90c4697539
Jeremiah Davis <jdavis@visgence.com>
2013-02-28 17:43:31 -0700

    [UNSTABLE!] Implementing check_access.

--- check_access.py
+++ check_access.py
@@ -1,11 +1,38 @@
-# Create your views here.
+'''
+' checkAccess.py
+' Contributing Authors:
+'    Jeremiah Davis (visgence, Inc)
+'
+' (c) 2013 Visgence, Inc.
+'''
+
+# System imports
 from django.http import HttpResponse
-from django.core import serializers
-from django.template import Context, loader
-from django.http import HttpResponseRedirect
-from django.utils.http import urlquote
+from django.contrib.auth.models import AnonymousUser
+try: import simplejson as json
+except ImportError: import json
+
+# Local imports
+from portcullis.models import PortcullisUser
 
 def check_access(request):
+    '''
+    ' If the request user resolves to a portcullisUser, return that.
+    ' If the request user is an AnonymousUser, return an 'Anonymous' PortcullisUser
+    '  (just a PortcullisUser Object with nothing set).
+    ' If the request user is not active, return an error.
+    '''
+    if not request.user.is_active:
+        return HttpResponse(json.dumps({'errors': 'Error: User is not active.'}), mimetype='application/json')
+
+    if isinstance(request.user, AnonymousUser):
+        return PortcullisUser(is_active=True)
+
+    try:
+        return request.user.portcullisuser
+    except Exception as e:
+        
+
 # TODO: Make useful for a single page app.
     if(request.user.username == ''):
         return HttpResponseRedirect("/portcullis/greeting/?next=%s" % urlquote(request.get_full_path()))
