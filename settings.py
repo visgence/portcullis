0d48f6172f619415b38beae37aa8a1423ca1b0b7
Bretton Murphy <bmurphy@visgence.com>
2013-03-05 15:49:13 -0700

    Make settings import database_settings or fail.

--- settings.py
+++ settings.py
@@ -13,16 +13,10 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'portcullis2',                      # Or path to database file if using sqlite3.
-        'USER': 'portcullis',                      # Not used with sqlite3.
-        'PASSWORD': 'p0rtcullis',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+try: 
+    from database_settings import *
+except:
+    raise Exception("Could not import database_settings!")
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
