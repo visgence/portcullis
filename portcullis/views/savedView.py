6b2b4dd155aaae0a449a3b6c055748dfe9bafb14
Jeremiah Davis <jdavis@visgence.com>
2013-03-07 16:48:46 -0700

    Keep track of zoom level in shared view.

--- portcullis/views/savedView.py
+++ portcullis/views/savedView.py
@@ -61,7 +61,7 @@ def savedView(request, token):
     return HttpResponse(t.render(c))
 
 @commit_on_success
-#@require_POST
+@require_POST
 def createSavedView(request):
     '''
     ' Create a savedView and return the token or link for that shared view.
@@ -79,7 +79,10 @@ def createSavedView(request):
     if 'jsonData' not in request.POST:
         raise Http404('Unrecognized data')
 
-    jsonData = json.loads(request.POST['jsonData'])
+    try:
+        jsonData = json.loads(request.POST['jsonData'])
+    except Exception as e:
+        return HttpResponse(json.dumps({'errors': 'Ivalid json: %s' % e.message}, mimetype="application/json"))
 
     expires = timezone.now() + timedelta(days=7)
     key = Key.objects.generateKey(portcullisUser, 'Saved view', expires, 20)
@@ -87,23 +90,42 @@ def createSavedView(request):
     # Create a SavedView object and graphs, and save data.
     savedView = SavedView.objects.create(key = key)
 
-    start = jsonData['start']
-    end = jsonData['end']
-    gran = jsonData['granularity']
-    # Not doing in a try catch, because should be gotten from existing graphs
+    try:
+        start = jsonData['start']
+        end = jsonData['end']
+        gran = jsonData['granularity']
+    except Exception as e:
+        message = 'Error getting json data: %s: %s:' % (type(e), e.message)
+        return HttpResponse(json.dumps({'errors': message}), mimetype="application/json")
+
     for graphData in jsonData['graphs']:
-        ds = DataStream.objects.get(id = graphData['ds_id'])
-       
+        try:
+            ds = DataStream.objects.get(id = graphData['ds_id'])
+        except DataStream.DoesNotExist:
+            return HttpResponse(json.dumps({'errors': 'Datastream does not exist.'}), mimetype="application/json")
+        except Exception as e:
+            return HttpResponse(json.dumps(
+                    {'errors': 'Unknown error occurred: %s: %s' % (type(e), e.message)},
+                    mimetype="application/json"))
+                               
         # Make sure not to add the key if not the owner.
         if key not in ds.can_read.all() and portcullisUser == ds.owner and not ds.is_public:
             ds.can_read.add(key)
-
+                               
+        try:
+            reduction = graphData['reduction']
+            zoom_start = graphData['zoom_start']
+            zoom_end = graphData['zoom_end']
+        except Exception as e:
+            message = 'Error getting json data for datastream %d: %s' % (ds.id, e.message)
+            return HttpResponse(json.dumps({'errors': message}), mimetype='application/json')
+        
         graph = SavedDSGraph(datastream = ds, start = start, end = end,
                              reduction_type = graphData['reduction'], granularity = gran,
-                             zoom_start = start, zoom_end = end)
+                             zoom_start = zoom_start, zoom_end = zoom_end)
         graph.save()
         savedView.widget.add(graph)
-
+        
     link = reverse('portcullis-saved-view', args = ['savedView', key.key])
 
     return HttpResponse(json.dumps({'html':'<a href="%s">%s</a>' % (link, link)}), mimetype='application/json')
