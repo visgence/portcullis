aef4cca83b79f6637ddcef1c66e06f9157602e7b
Jeremiah Davis <jdavis@visgence.com>
2013-03-11 18:08:57 -0600

    Fix redirect and urls error for django 1.5

--- graphs/urls.py
+++ graphs/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.views.generic.simple import redirect_to
+from django.views.generic import RedirectView
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -8,7 +8,7 @@ urlpatterns = patterns('graphs.views',
     url(r'^$', 'display_graphs.display_graph'),
     url(r'^render_container/$', 'display_graphs.render_graph_container'),
     url(r'^render_graph/$', 'display_graphs.render_graph'),
-    url(r'^favicon\.ico$', redirect_to, {'url': '/static/portcullis/favicon.ico'}),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/portcullis/favicon.ico')),
     url(r'^scaling_functions.js/$', 'scaling_functions.scaling_functions'),
     url(r'^sharedGraph/(?P<token>.+)/(?P<id>\d{1,4})/$', 'display_graphs.shared_graph'),
 )
