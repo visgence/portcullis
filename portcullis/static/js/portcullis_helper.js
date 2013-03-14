fc116855e3c6b210a0175a9c1e59a80be0a099eb
Jeremiah Davis <jdavis@visgence.com>
2013-03-13 18:34:02 -0600

    Add func to create a dialog from a server view.

--- portcullis/static/js/portcullis_helper.js
+++ portcullis/static/js/portcullis_helper.js
@@ -11,8 +11,9 @@
 
 /** Set up a spin indicator */
 function spin(target, type) {
+    var opts = null;
     if ( type == 'tiny' ) {
-        var opts = {
+        opts = {
             lines: 9,
             length: 4,
             width: 1,
@@ -31,7 +32,7 @@ function spin(target, type) {
         };
     }
     else {
-        var opts = {
+        opts = {
             lines: 20,
             length: 50,
             width: 4,
@@ -53,4 +54,60 @@ function spin(target, type) {
     var spinIndicator = new Spinner(opts);
     spinIndicator.spin(target);
     return spinIndicator;
+}
+
+/** This method will create a dialogue and insert content from an ajax call
+ *  into it.
+ *
+ * \param[in] view  The view to load into the dialog.
+ * \param[in] title The title on the dialog.
+ */
+function makeDialog(view, title)
+{
+    $('body').append('<div id="serverDialog"></div>');
+    var div = $('#serverDialog');
+    $(div).addClass('serverDialog');
+
+    // Find all the serverDialogs, and find the one with the largest id
+    var IDs = $.map($('.serverDialog'), function(e, i) {
+        return $(e).data('id');
+    });
+
+    console.log(IDs);
+
+    var data_id = Math.max.apply(null,IDs) + 1;
+    var id = 'serverDialog' + data_id;
+
+    $(div).data('id', data_id);
+    $(div).attr('id', id);
+    $(div).css('display', 'none');
+    $(div).attr('title', title);
+
+
+    $('#' + id).dialog({
+        autoOpen: true,
+        resizable: true,
+        hide: 'fade',
+        show: 'fade',
+        modal: false,
+        minWidth: 300,
+        maxWidth: 1000,
+        minHeight: 300,
+        maxHeight: 1000,
+        dialogClass: "non-modal dialogue",
+        close: function() {
+            $(this).dialog('destroy');
+            $('#' + id).remove();
+        }
+    });
+
+    $.get(view, {}, function(resp) {
+        console.log('Setting errors');
+        if ( resp.errors ) {
+            console.log(resp.errors);
+        }
+        console.log('Setting dialog html');
+        $('#'+id).html(resp.html);
+    });
+
 }
