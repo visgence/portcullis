af7ebdf9847752c3daf7a8b82e646ae100a13f7a
Jeremiah Davis <jdavis@visgence.com>
2013-03-14 14:43:05 -0600

    Fix style errors

--- portcullis/static/js/login.js
+++ portcullis/static/js/login.js
@@ -37,3 +37,74 @@ function post_login(data)
 }
 
 
+/** This method will pop up a dialog that will allow the user to change their password.
+  *
+  */
+function changePassDialog()
+{
+    var ok = {
+        text: 'OK',
+        click: function() {
+            $(this).dialog('close');
+        }
+    };
+
+    var buttons = [
+        {
+            text: 'Change Password',
+            click: function() {
+                // Send the change password request
+                var postData = {};
+                var self = this;
+                postData.oldPassword = $('#oldPassword').val();
+                postData.newPassword = $('#passwordInput').val();
+                
+                if ( postData.oldPassword === '' ) {
+                    makeDialog('Please enter your old password', 'Error', [ok]);
+                    return;
+                }
+                if ( postData.newPassword === '' ) {
+                    makeDialog('Please enter your new password', 'Error', [ok]);
+                    return;
+                }
+                if ( postData.newPassword !== $('#passwordReinput').val() ) {
+                    makeDialog('Your new passwords do not match.  Please reenter them.', 'Error', [ok]);
+                    return;
+                }
+                var csrf = $('input[name="csrfmiddlewaretoken"]').val();
+                $.post('/portcullis/changePassword', {csrfmiddlewaretoken: csrf,
+                                                      jsonData: JSON.stringify(postData)},
+                       function(resp) {
+                           if ( resp.errors ) {
+                               makeDialog(resp.errors, 'Error', [ok]);
+                           }
+                           else if ( resp.success ) {
+                               makeDialog(resp.success, 'Success', [ok]);
+                               $(self).dialog('close');
+                           }
+                           else {
+                               makeDialog(resp, 'Unknown Error', [ok]);
+                           }
+                       });
+            }
+        },
+        {
+            text: 'Cancel',
+            click: function() {
+                $(this).dialog('close');
+            }
+        }
+    ];
+    
+    $.get('/portcullis/passwordForm/', {}, function(resp) {
+        if ( resp.errors ) {
+            makeDialog(resp.errors, 'Error', [{text:'OK', click: function(){$(this).dialog('close');}}]);
+        }
+        else if ( resp.html ) {
+            makeDialog(resp.html, 'Change Password', buttons);
+        }
+        else {
+            makeDialog(resp, 'Unknown Error', [{text:'OK', click: function(){$(this).dialog('close');}}]);
+        }
+    });
+}
