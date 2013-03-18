6fb38ebed347c1bebacebab0139444927205f5c6
Bretton Murphy <bmurphy@visgence.com>
2013-03-18 14:22:11 -0600

    Remove database settings and put in settings.

--- settings.py
+++ settings.py
@@ -19,7 +19,19 @@ DT_FORMAT = "%m/%d/%Y %H:%M:%S"
 #Import the database settings stored locally.  This allows for different databases
 #under different dev environments without impacting production.
 
-from database_settings import *
+try:
+    from local_database_settings import *
+except:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'portcullis.db',                      # Or path to database file if using sqlite3.
+            'USER': '',                      # Not used with sqlite3.
+            'PASSWORD': '',                  # Not used with sqlite3.
+            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        }
+    }
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
