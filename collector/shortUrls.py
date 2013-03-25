bfcbc1be2c6891cd8e2386890cdf1da9abf87ceb
Jeremiah Davis <jdavis@visgence.com>
2013-03-25 13:37:06 -0600

    Allow keys > 16 digits for /api/add/.

--- collector/shortUrls.py
+++ collector/shortUrls.py
@@ -4,5 +4,5 @@ urlpatterns = patterns('collector.views',
 #    url(r'^add_reading/$','data_loader.add_reading'),
 #    url(r'^add_reading_bulk/$','data_loader.add_reading_bulk'),
 #    url(r'^add_reading_bulk_hash/$','data_loader.add_reading_bulk_hash'),
-    url(r'^add/(?P<auth_token>.{1,16})$', 'data_loader.add_list'),
+    url(r'^add/(?P<auth_token>.+)$', 'data_loader.add_list'),
 )
