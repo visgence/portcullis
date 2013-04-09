89fbff72ccceabfcdbfbd6c2b6a12164a722671d
Bretton Murphy <bmurphy@visgence.com>
2013-04-09 15:27:17 -0600

    Make Datastream create view take a list of ds data.

--- datastreams/views/create.py
+++ datastreams/views/create.py
@@ -2,6 +2,7 @@
 " datastreams/views/create.py
 " Contributing Authors:
 "    Bretton Murphy (Visgence, Inc)
+"    Jerry Davis (Visgence, Inc)
 "
 " (c) 2012 Visgence, Inc.
 """
@@ -16,75 +17,94 @@ try:
 except ImportError:
     import json
 
+from django.db import transaction
+
 #Local Imports
 from portcullis.models import DataStream, Key, ScalingFunction
 
 
 @require_POST
 @csrf_exempt
+@transaction.commit_manually
 def createDs(request):
     '''
-    ' Creates a DataStream with data from json and with the owner of the specified token.
+    ' Creates DataStreams with an array of data from json and with the owner of the specified token.
     '
     ' The json required format is the following:
-    '   {
+    '   [{
     '       "token"  : key token,
     '       "ds_data": Dictionary of key value pairs. Keys are names that match datastream field names and
     '                  value is the value to set to the new object.
-    '   }
+    '   }, etc]
     '
     ' This json is then given to a dictionary to be sent as the request like so:
     '   { "jsonData": json_stuff }
     '
-    ' Returns: HttpResponse with Json containing the new DataStreams id or error is anything went wrong.
+    ' Returns: HttpResponse with Json containing a list with the new DataStream's ids or error is anything went wrong.
     '''
 
     #TODO: for now screw permissions, but later, put them in!!
-
+    print request.POST
     try:
         json_data = json.loads(request.POST.get("jsonData"))
     except Exception as e:
+        transaction.rollback()
         return get_http_response("From createDs: Problem getting json data from request.", str(e))
+    
+    #jsonData should contain a list of data.
+    if not isinstance(json_data, list):
+        transaction.rollback()
+        return get_http_response("From createDs: json data is not a list.", '')
 
-    try:
-        key = Key.objects.get(key=json_data['token'])
-    except Key.DoesNotExist as e:
-        return get_http_response("From createDs: Key with token '%s' does not exist." % str(json_data['token']), str(e))
-    except KeyError as e:
-        return get_http_response("From createDs: 'token' does not exist in the json data.", str(e))
-
-    try:
-        ds_name = json_data['ds_data']['name']
-    except Exception as e:
-        return get_http_response("From createDs: Exception getting DS name: %s." % type(e), str(e))
-
-    try:
-        ds = DataStream.objects.get(name=ds_name, owner=key.owner)
-    except DataStream.DoesNotExist:
-        ds = DataStream(owner=key.owner)
-        for attr, val in json_data['ds_data'].iteritems():
-            if attr == "scaling_function":
-                try:
-                    sc = ScalingFunction.objects.get(name=val)
-                except ScalingFunction.DoesNotExist as e:
-                    return get_http_response("From createDs: scaling function with name '%s' does not exist." % str(val), str(e))
-                setattr(ds, attr, sc)
-            else:
-                try:
-                    setattr(ds, attr, val)
-                except Exception as e:
-                    error = "From createDs: There was a problem setting '%s' with the value '%s'." % (str(attr), str(val))
-                    return get_http_response(error, str(e))
+    return_data = []
+    for data in json_data:
+        print data 
         try:
-            ds.full_clean()
-            ds.save()
-        except ValidationError as e:
-            return get_http_response("From createDs: There were one or more problems setting DataStream attributes.", str(e))
+            key = Key.objects.get(key=data['token'])
+        except Key.DoesNotExist as e:
+            transaction.rollback()
+            return get_http_response("From createDs: Key with token '%s' does not exist." % str(data['token']), str(e))
+        except KeyError as e:
+            transaction.rollback()
+            return get_http_response("From createDs: 'token' does not exist in the json data.", str(e))
 
-    return_data = {'id': ds.pk}
-    ds.can_read.add(key)
-    ds.can_post.add(key)
+        try:
+            ds_name = data['ds_data']['name']
+        except Exception as e:
+            transaction.rollback()
+            return get_http_response("From createDs: Exception getting DS name: %s." % type(e), str(e))
 
+        try:
+            ds = DataStream.objects.get(name=ds_name, owner=key.owner)
+        except DataStream.DoesNotExist:
+            ds = DataStream(owner=key.owner)
+            for attr, val in data['ds_data'].iteritems():
+                if attr == "scaling_function":
+                    try:
+                        sc = ScalingFunction.objects.get(name=val)
+                    except ScalingFunction.DoesNotExist as e:
+                        transaction.rollback()
+                        return get_http_response("From createDs: scaling function with name '%s' does not exist." % str(val), str(e))
+                    setattr(ds, attr, sc)
+                else:
+                    try:
+                        setattr(ds, attr, val)
+                    except Exception as e:
+                        transaction.rollback()
+                        error = "From createDs: There was a problem setting '%s' with the value '%s'." % (str(attr), str(val))
+                        return get_http_response(error, str(e))
+            try:
+                ds.full_clean()
+                ds.save()
+            except ValidationError as e:
+                transaction.rollback()
+                return get_http_response("From createDs: There were one or more problems setting DataStream attributes.", str(e))
+
+        return_data.append(ds.pk)
+        ds.can_read.add(key)
+        ds.can_post.add(key)
+
+    transaction.commit()
     return HttpResponse(json.dumps(return_data), mimetype="application/json")
 
 
