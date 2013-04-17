e096c73028d61f94bf79da91f3c08d290bc28719
Bretton Murphy <bmurphy@visgence.com>
2013-04-17 12:20:13 -0600

    Add method to utilites for HttpResponse without json.

--- api/utilites.py
+++ api/utilites.py
@@ -14,9 +14,10 @@ except ImportError:
     import json
 
 
-def cors_http_response(content):
+def cors_http_response_json(content):
     '''
-    ' Creates and returns a HttpRespone object that allows cross site access through Cors.
+    ' Creates and returns a HttpRespone object that allows cross site access through Cors 
+    ' with json serialized content.
     '
     ' Keyword Arguements:
     '   content - Object that is json serialized into the content of the HttpResponse object.
@@ -25,5 +26,19 @@ def cors_http_response(content):
     '''
 
     resp = HttpResponse(json.dumps(content), mimetype = "application/json")
+    resp['Access-Control-Allow-Origin'] = '*'
+    return resp
+
+def cors_http_response(content):
+    '''
+    ' Creates and returns a HttpRespone object that allows cross site access through Cors.
+    '
+    ' Keyword Arguements:
+    '   content - String that will become the content of the HttpResponse
+    '
+    ' Return: HttpResponse that allows cross site access with string content
+    '''
+
+    resp = HttpResponse(content, mimetype = "text/html")
     resp['Access-Control-Allow-Origin'] = '*'
     return resp
