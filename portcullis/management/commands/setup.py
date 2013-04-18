494ebb01cc5c236fd9682b9869b3d8ae5c33753b
Jeremiah Davis <jdavis@visgence.com>
2013-04-18 13:26:13 -0600

    Comment out git import, so gitPython not necessary for command.

--- portcullis/management/commands/setup.py
+++ portcullis/management/commands/setup.py
@@ -11,7 +11,7 @@ from django.core.management import call_command
 from django.core.management.base import BaseCommand
 from settings import APP_PATH
 from shutil import copyfile
-import git
+#import git
 import sys
 
 # local imports
