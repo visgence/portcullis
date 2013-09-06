cc770d0a6870c43bb5ccae159ba53d0472cfb05e
djsubaku@gmail.com <bmurphy@visgence.com>
2013-09-06 12:46:55 -0600

    Add more unit tests for sensor

--- api/tests.py
+++ api/tests.py
@@ -26,115 +26,8 @@ from graphs.models import DataStream, SensorReading, Sensor, ClaimedSensor
 from portcullis.models import PortcullisUser
 from portcullis.customExceptions import SensorReadingCollision
 from api.views.reading_loader import insert_reading
-from api.views.sensor import claimSensor
+from api.views.sensor import claimSensor, create
 
-
-class CreateTest(TestCase):
-    '''
-    ' Tests for the view file create.py
-    '''
-
-    fixtures = ['test_keys.json', 'test_scalingFunctions.json', 'test_portcullisUsers.json', 'test_dataStreams.json']
-
-    def setUp(self):
-        self.json_data = {
-            'name': "new stream",
-            'scaling_function': "Identity", 
-            'reduction_type': "mean",
-            'is_public': True,
-        }
-
-        self.c = Client()
-
-    def get_json(self, token):
-        '''
-        ' Helper method for tests that prep's the json data for createDs
-        '''
-
-        data = {
-            'token': token,
-            'ds_data': self.json_data
-        }
-        return json.dumps(data)
-
-
-
-    ################### createDs ###################
-    
-    def test_createDs_keyDoesNotExist(self):
-        '''
-        ' If given a key token that does not exist we should get back json with a specific
-        ' error message.
-        '''
-
-        json_data = self.get_json("bad token")
-        response = self.c.post('/api/create_ds/', {'jsonData':json_data})
-        response_json = json.loads(response.content)
-        self.assertEquals(response_json['error'], "From createDs: Key with token 'bad token' does not exist.")
-      
-    def test_createDs_badJsonKey(self):
-        '''
-        ' If the post request can't find jsonData then we should get back json with a specific error.
-        '''
-
-        json_data = self.get_json("bad token")
-        response = self.c.post('/api/create_ds/', {'bad_key':json_data})
-        response_json = json.loads(response.content)
-        self.assertEqual(response_json['error'], "From createDs: Problem getting json data from request.")
-
-    def test_createDs_streamExists(self):
-        '''
-        ' We should get back the id of a existant datastream if we try to create one with the same owner and name.
-        '''
-
-        ds = DataStream.objects.get(pk=1, name="superuser_ds1")
-        self.json_data['name'] = ds.name
-        json_data = self.get_json("superuser_key1")
-        response = self.c.post('/api/create_ds/', {'jsonData':json_data})
-        response_json = json.loads(response.content)
-        same_ds = DataStream.objects.get(pk=response_json['id'])
-        
-        self.assertEquals(same_ds, ds)
-        
-    def test_createDs_newStream(self):
-        '''
-        ' We should get the id of a newly created DataStream if we give a proper json information.
-        '''
-
-        #First make sure the ds does not exist yet.
-        owner = AuthUser.objects.get(pk=1)
-        self.assertRaises(DataStream.DoesNotExist, DataStream.objects.get, owner=owner, name=self.json_data['name'])
-        json_data = self.get_json("superuser_key1")
-        response = self.c.post('/api/create_ds/', {'jsonData': json_data})
-        response_json = json.loads(response.content)
-        new_ds = DataStream.objects.get(pk=response_json['id'])        
-        self.assertEqual(new_ds, DataStream.objects.get(owner=owner, name=self.json_data['name']))
-
-    def test_createDs_badScalingFunction(self):
-        '''
-        ' If we give a bad scaling function name in the json data we should get back a specific
-        ' error message
-        '''
-       
-        self.json_data['scaling_function'] = "bad sc name"
-        json_data = self.get_json("superuser_key1")
-        response = self.c.post('/api/create_ds/', {'jsonData':json_data})
-        response_json = json.loads(response.content)
-        
-        self.assertEqual(response_json['error'], "From createDs: scaling function with name 'bad sc name' does not exist.")
-    
-    def test_createDs_badNameAttr(self):
-        '''
-        ' If we give an attribute for a new datastream that is not right, in this case setting owner to and None, we should
-        ' get an error back.
-        '''
-
-        self.json_data['reduction_type'] = "bad choice" 
-        json_data = self.get_json("superuser_key1")
-        response = self.c.post('/api/create_ds/', {'jsonData':json_data})
-        response_json = json.loads(response.content)
-        
-        self.assertEqual(response_json['error'], "From createDs: There were one or more problems setting DataStream attributes.")
 
 
 class ReadingLoaderTest(TestCase):
@@ -193,6 +86,7 @@ class ReadingLoaderTest(TestCase):
         self.assertRaises(SensorReadingCollision, insert_reading, *(ds, sensor, 10.5, timestamp) )
 
 
+
 class SensorTest(TestCase):
     '''
     '  Tests for the view file sensor.py
@@ -243,8 +137,9 @@ class SensorTest(TestCase):
         sensor = Sensor.objects.get(uuid="sensor_one_id")
         name = "Claimed Sensor One"
         cs = claimSensor(sensor, name, None)
-        self.assertTrue(isinstance(cs, list))
-    
+        self.assertTrue(isinstance(cs, str))
+   
+
     def test_claimSensor_update_sensor(self):
         '''
             Test that we get back the same ClaimedSensor we are updating the sensor on.
@@ -259,6 +154,7 @@ class SensorTest(TestCase):
         self.assertTrue(isinstance(csTwo, ClaimedSensor))
         self.assertEqual(csOne.pk, csTwo.pk)
         self.assertNotEqual(csOne.sensor, csTwo.sensor)
+
 
     def test_claimSensor_create(self):
         '''
@@ -282,4 +178,86 @@ class SensorTest(TestCase):
         self.assertEqual(cs.pk, newCs.pk)
 
 
+    def test_create_no_owner(self):
+        '''
+            Test that we get back an error should we not give an owner
+        '''
 
+        data = {"uuid": "sensor_one_id", "name": "Claimed Sensor One"} 
+        self.assertTrue(isinstance(create(data, None), str))
+
+
+    def test_create_no_uuid(self):
+        '''
+            Test that we get back an error should we not send a uuid 
+        '''
+
+        data = {"name": "Claimed Sensor One"} 
+        owner = PortcullisUser.objects.get(email="admin@visgence.com")
+        self.assertTrue(isinstance(create(data, owner), str))
+    
+    
+    def test_create_no_name(self):
+        '''
+            Test that we get back an error should we not send a name
+        '''
+
+        data = {"uuid": "sensor_one_id"} 
+        owner = PortcullisUser.objects.get(email="admin@visgence.com")
+        self.assertTrue(isinstance(create(data, owner), str))
+
+
+    def test_create_bad_scaling_function(self):
+        '''
+            Test that we get an error if we an error if we try to give a non existant scaling function name
+        '''
+
+        data = {"uuid": "sensor_one_id", "name": "Claimed Sensor One", "scaling_function": "false name"} 
+        owner = PortcullisUser.objects.get(email="admin@visgence.com")
+        self.assertTrue(isinstance(create(data, owner), str))
+        
+
+    def test_create_good_data_create(self):
+        '''
+            Test that we get back a new sensor given good data
+        '''
+
+        data = {"uuid": "brand_new_sensor", "name": "Brand New Name"} 
+        owner = PortcullisUser.objects.get(email="admin@visgence.com")
+        sensor = create(data, owner)
+        self.assertTrue(isinstance(sensor, Sensor))
+        self.assertEqual(Sensor.objects.get(uuid="brand_new_sensor"), sensor)
+
+
+
+    def test_create_good_data_update(self):
+        '''
+            Test that we get back a updated sensor given good data
+        '''
+
+        data = {"uuid": "sensor_one_id", "name": "Brand New Name", "units": "new units"} 
+        owner = PortcullisUser.objects.get(email="admin@visgence.com")
+        sensor = Sensor.objects.get(uuid="sensor_one_id")
+        updatedSensor = create(data, owner)
+
+        self.assertTrue(isinstance(updatedSensor, Sensor))
+        self.assertNotEqual(updatedSensor.units, sensor.units)
+
+
+    def test_create_swap_sensor(self):
+        '''
+            Test that we can claim a new sensor.
+        '''
+
+        data = {"uuid": "brand_new_sensor", "name": "Claimed Sensor One"} 
+        owner = PortcullisUser.objects.get(email="admin@visgence.com")
+
+        claimedSensor = ClaimedSensor.objects.get(owner=owner, name=data['name'])
+        oldSensor = Sensor.objects.get(uuid="sensor_one_id")
+        self.assertEqual(claimedSensor.sensor, oldSensor)
+
+        newSensor = create(data, owner)
+
+        self.assertNotEqual(newSensor, oldSensor)
+        self.assertEqual(ClaimedSensor.objects.get(sensor=newSensor), claimedSensor)
+        
