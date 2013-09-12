4f96e245dbeef34629511bf89149e3eadf678131
djsubaku@gmail.com <bmurphy@visgence.com>
2013-09-12 12:18:55 -0600

    Start Refactoring how nav bar and url hash works

--- portcullis/static/js/controller.js
+++ portcullis/static/js/controller.js
@@ -1,45 +1,46 @@
-var state_cach = {};
 
-/* Responds to any changes in the url hash for jquery bbq */
-$(window).bind('hashchange', function(event) {
-    var state = event.getState();
-   
-    if('tab' in state) {
-        var newTab = $('.navbar-nav li[data-target="'+state['tab']+'"]');
-        if(newTab.length <= 0)
-            return;
-        
-        var current = $('.navbar-nav li.active');
-        if(state['tab'] === $(current).data('target'))
-            return;
+$(function() {
+    var state_cach = {};
 
-        $('.'+$(current).data('target')).hide();
-        $(current).removeClass('active');
-        $(window).trigger(current+'-hidden');
+    /* Responds to any changes in the url hash for jquery bbq */
+    $(window).bind('hashchange', function(event) {
+        var state = event.getState();
+    
+        if('tab' in state) {
+            if($.fn.Nav.isTab(state['tab']))
+                return;
+            
+            var current = $.fn.Nav.getActive();
+            if(state['tab'] === current)
+                return;
 
-        $('.navbar-nav li[data-target="'+state['tab']+'"]').addClass('active');
-        $('.'+state['tab']).show();
-        $(window).trigger(state['tab']+'-shown');
-    }
-
-    if('time' in state) {
-        $('#'+state['time']).attr('checked', 'checked');
-
-        if(state['time'] == "custom") {
-            if('start' in state)
-                $('#start').val(state['start']);
-            if('end' in state)
-                $('#end').val(state['end']);
+            $.fn.Nav.activate(current);
+        }
+        else if($.fn.Nav.isActive('graphs') === false) {
+            $.fn.Nav.activate('graphs');
         }
 
-        $('#'+state['time']).trigger('change');
-    }
+        if('time' in state) {
+            $('#'+state['time']).attr('checked', 'checked');
 
-    if('auto-refresh' in state && state['auto-refresh'] == "true") {
-        $('#auto_refresh').attr('checked', 'checked');
-        $('#auto_refresh').trigger('change');
-    }
-    else {
-        $('#auto_refresh').removeAttr('checked');
-    }
+            if(state['time'] == "custom") {
+                if('start' in state)
+                    $('#start').val(state['start']);
+                if('end' in state)
+                    $('#end').val(state['end']);
+            }
+
+            $('#'+state['time']).trigger('change');
+        }
+
+        if('auto-refresh' in state && state['auto-refresh'] == "true") {
+            $('#auto_refresh').attr('checked', 'checked');
+            $('#auto_refresh').trigger('change');
+        }
+        else {
+            $('#auto_refresh').removeAttr('checked');
+        }
+    });
+
+    $(window).trigger('hashchange');
 });
---
+++ portcullis/static/js/nav.js
@@ -0,0 +1,81 @@
+$(function() {
+    var stateCache = {};
+    var currentRequest = null;
+    var tabUris = {
+        'graphs': '/graphs/index'
+    };
+    var properStates = {
+        'graphs':['time', 'auto-refresh', 'start', 'end'], 
+        'utilities': []
+    };
+
+    $('ul.navbar-nav').on('click', 'li', function(e) {
+        var newTab = $(e.currentTarget).data('target');
+        if(isActive(newTab) === false)
+            activate(newTab);
+    });
+
+    var changeHashTab = function(newTab) {
+
+        var state = $.bbq.getState();
+        
+        var currentTab = 'graphs';
+        if(state.hasOwnProperty('tab'))
+            currentTab = state['tab'];
+
+        var stateToSave = {};
+        $.each(state, function(key, val) {
+            if($.inArray(key, properStates[currentTab]) > -1)
+                stateToSave[key] = val;
+        });
+        stateCache[currentTab] = stateToSave;
+        
+        var newState = {
+            'tab': newTab
+        };
+        if(stateCache.hasOwnProperty(newTab) && stateCache[newTab] !== undefined)
+            $.extend(true, newState, stateCache[newTab]);
+
+        $.bbq.pushState(newState, 2);
+    };
+
+    var isTab = function(tab) {
+        tab = tab || '';
+        return $('ul.navbar-nav li[data-target="'+tab+'"]').length > 0 ? true:false;
+    };
+
+    var isActive = function(tab) {
+        tab = tab || '';
+        return $('ul.navbar-nav li[data-target="'+tab+'"]').hasClass('active');
+    };
+
+    var getActive = function() {
+        return $('ul.navbar-nav li.active').data('target');
+    };
+
+    var activate = function(tab) {
+        tab = tab || '';
+        var toActivate = $('ul.navbar-nav li[data-target="'+tab+'"]');
+        if(toActivate.length > 0) {
+            $('ul.navbar-nav li[class="active"]').removeClass('active');
+            $(toActivate).addClass('active');
+            changeHashTab(tab);
+            loadActive();
+        }
+    };
+
+    var loadActive = function() {
+        var active = getActive();
+        if(tabUris.hasOwnProperty(active)) {
+            $.get(tabUris[active], function(resp) {
+                $('#base-content').html(resp);
+            });
+        }
+    };
+
+    $.fn.Nav = {
+        'isActive': isActive
+        ,'activate': activate
+        ,'isTab': isTab
+    };
+});
