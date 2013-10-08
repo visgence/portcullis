7d24860bbc60c967b12f575d4ba409d6981d8bc3
djsubaku@gmail.com <bmurphy@visgence.com>
2013-10-08 12:49:45 -0600

    Change modal dialog for password change to use bootstrap's modal dialog

--- portcullis/static/js/login.js
+++ portcullis/static/js/login.js
@@ -32,75 +32,72 @@ function post_login(data)
 }
 
 
-/** This method will pop up a dialog that will allow the user to change their password.
-  *
-  */
 function changePassDialog()
 {
-    var ok = {
-        text: 'OK',
-        click: function() {
-            $(this).dialog('close');
-        }
-    };
 
-    var buttons = [
-        {
-            text: 'Change Password',
-            click: function() {
-                // Send the change password request
-                var postData = {};
-                var self = this;
-
-                postData.oldPassword = $('#oldPassword').val();
-                postData.newPassword = $('#passwordInput').val();
-
-                if ( postData.oldPassword === '' ) {
-                    makeDialog('Please enter your old password', 'Error', [ok]);
-                    return;
-                }
-                if ( postData.newPassword === '' ) {
-                    makeDialog('Please enter your new password', 'Error', [ok]);
-                    return;
-                }
-                if ( postData.newPassword !== $('#passwordReinput').val() ) {
-                    makeDialog('Your new passwords do not match.  Please reenter them.', 'Error', [ok]);
-                    return;
-                }
-                var csrf = $('input[name="csrfmiddlewaretoken"]').val();
-                $.post('/api/changePassword/', {csrfmiddlewaretoken: csrf,
-                                                      jsonData: JSON.stringify(postData)},
-                       function(resp) {
-                           if ( resp.errors ) {
-                               makeDialog(resp.errors, 'Error', [ok]);
-                           }
-                           else if ( resp.success ) {
-                               makeDialog(resp.success, 'Success', [ok]);
-                               $(self).dialog('close');
-                           }
-                           else {
-                               makeDialog(resp, 'Unknown Error', [ok]);
-                           }
-                       });
+    if(!$('#change-pass-modal').length) {
+        
+        $.get('/api/passwordForm/', {}, function(resp) {
+            
+            if ( resp.errors ) {
+                sysDialog(resp.errors, "Error", 'error')
             }
-        },
-        {
-            text: 'Cancel',
-            click: function() {
-                $(this).dialog('close');
+            else if ( resp.html ) {
+                $(resp.html).modal({
+                    backdrop: false
+                });
+
+                $('#change-pass-btn').on('click', function() { 
+                    $('#pass-msg').removeClass('alert-danger alert').html('');
+
+                    // Send the change password request
+                    var postData = {};
+                    var self = this;
+
+                    postData.oldPassword = $('#oldPassword').val();
+                    postData.newPassword = $('#passwordInput').val();
+
+                    if ( postData.oldPassword === '' ) {
+                        $('#pass-msg').addClass('alert-danger alert').html('Please enter your old password');
+                        return;
+                    }
+                    if ( postData.newPassword === '' ) {
+                        $('#pass-msg').addClass('alert-danger alert').html('Please enter your new password');
+                        return;
+                    }
+                    if ( postData.newPassword !== $('#passwordReinput').val() ) {
+                        var error = 'Your new passwords do not match.  Please reenter them.';
+                        $('#pass-msg').addClass('alert-danger alert').html(error);
+                        return;
+                    }
+                    var csrf = $('input[name="csrfmiddlewaretoken"]').val();
+                    $.post('/api/changePassword/', {csrfmiddlewaretoken: csrf, jsonData: JSON.stringify(postData)}, function(resp){
+                        if ( resp.errors ) {
+                            $('#pass-msg').addClass('alert-danger alert').html(resp.errors);
+                        }
+                        else if ( resp.success ) {
+                            $('#change-pass-modal').modal('hide');
+                            sysDialog('You have successfully changed your password', 'Success', 'success');
+                        }
+                        else {
+                            $('#pass-msg').addClass('alert-danger alert').html(resp);
+                        }
+                    });
+                    return false;
+                });
+
+                $('#change-pass-modal').on('hidden.bs.modal', function() {
+                    $('#pass-msg').removeClass('alert-danger alert').html('');
+                    $('#change-pass-modal form').get(0).reset();
+                });
             }
-        }
-    ];
-    
-    $.get('/api/passwordForm/', {}, function(resp) {
-        if ( resp.errors ) {
-            makeDialog(resp.errors, 'Error', [{text:'OK', click: function(){$(this).dialog('close');}}]);
-        }
-        else if ( resp.html ) {
-            makeDialog(resp.html, 'Change Password', buttons);
-        }
-        else {
-            makeDialog(resp, 'Unknown Error', [{text:'OK', click: function(){$(this).dialog('close');}}]);
-        }
-    });
+            else {
+                sysDialog(resp, 'Unknown Error', 'error');
+            }
+        });
+    }
+    else {
+        $("#change-pass-modal").modal('show');    
+    }
 }
+
