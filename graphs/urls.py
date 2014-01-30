e19b1e9bc16a11cc89c1e646664519a0d441fa9c
Jeremiah Davis <jdavis@visgence.com>
2014-01-30 14:45:48 -0700

    Remove deprecated defaults.

--- graphs/urls.py
+++ graphs/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 from django.views.generic import RedirectView
 # Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
+# from django.contrib import admin
+# admin.autodiscover()
 
 urlpatterns = patterns(
     'graphs.views',
